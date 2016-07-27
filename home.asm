6babc4b87e054bd0f2b6cfd0fbafe14993ec2bc1
wolfboyft <henryfthomson@gmail.com>
2016-07-27 12:00:52 +0100

    Reformat home.asm.

--- home.asm
+++ home.asm
@@ -1,49 +1,23 @@
-;****************************************************************************************************************************************************
-;*	Blank Simple Source File
-;*
-;****************************************************************************************************************************************************
-;*
-;*
-;****************************************************************************************************************************************************
+INCLUDE	"hardware.asm"
+INCLUDE "rst.asm"
+INCLUDE "bankmacros.asm"
+INCLUDE "charmap.asm"
+INCLUDE "textmacros.asm"
+INCLUDE "constants.asm"
 
-;****************************************************************************************************************************************************
-;*	Includes
-;****************************************************************************************************************************************************
-
-	INCLUDE	"hardware.asm"
-	INCLUDE "rst.asm"
-	INCLUDE "bankmacros.asm"
-	INCLUDE "charmap.asm"
-	INCLUDE "textmacros.asm"
-	INCLUDE "constants.asm"
-
-;****************************************************************************************************************************************************
-;*	user data (constants)
-;****************************************************************************************************************************************************
-
-
-;****************************************************************************************************************************************************
-;*	equates
-;****************************************************************************************************************************************************
-
-
-;****************************************************************************************************************************************************
-;*	cartridge header
-;****************************************************************************************************************************************************
-
-	SECTION	"Org $00",HOME[$00]
+SECTION	"Org $00", HOME[$00]
 RST_00:
 	ld [Buffer], a
 	jp Farcall
 
-	SECTION	"Org $08",HOME[$08]
+SECTION	"Org $08", HOME[$08]
 RST_08:
 	ld [ROMBank], a
 	ld [$2000], a
 	ret
 	; no need for anything along the lines of jp Bankswitch, it's all here
 
-	SECTION	"Org $10",HOME[$10]
+SECTION	"Org $10", HOME[$10]
 RST_10:
 	push de
 	ld e, a
@@ -57,11 +31,11 @@ endr
 	pop de
 	jp [hl]
 
-;	SECTION	"Org $18",HOME[$18]
+;	SECTION	"Org $18", HOME[$18]
 ;RST_18:
 ;	jp	$100
 
-	SECTION	"Org $20",HOME[$20]
+SECTION	"Org $20", HOME[$20]
 RST_20:
 	push de
 	ld e, a
@@ -75,39 +49,39 @@ endr
 	pop de
 	ret
 
-;	SECTION	"Org $28",HOME[$28]
+;	SECTION	"Org $28", HOME[$28]
 ;RST_28:
 ;	jp	$100
 ;
-	SECTION	"Org $30",HOME[$30]
+SECTION	"Org $30", HOME[$30]
 RST_30:
 	jp	$100
 
-	SECTION	"Org $38",HOME[$38]
+SECTION	"Org $38", HOME[$38]
 RST_38:
 	jp	$100
 
-	SECTION	"V-Blank IRQ Vector",HOME[$40]
+SECTION	"V-Blank IRQ Vector", HOME[$40]
 VBL_VECT:
 	jp VBlank
 
-	SECTION	"LCD IRQ Vector",HOME[$48]
+SECTION	"LCD IRQ Vector", HOME[$48]
 LCD_VECT:
 	reti
 
-	SECTION	"Timer IRQ Vector",HOME[$50]
+SECTION	"Timer IRQ Vector", HOME[$50]
 TIMER_VECT:
 	reti
 
-	SECTION	"Serial IRQ Vector",HOME[$58]
+SECTION	"Serial IRQ Vector", HOME[$58]
 SERIAL_VECT:
 	reti
 
-	SECTION	"Joypad IRQ Vector",HOME[$60]
+SECTION	"Joypad IRQ Vector", HOME[$60]
 JOYPAD_VECT:
 	reti
 
-	SECTION	"Start",HOME[$100]
+SECTION	"Start", HOME[$100]
 	nop
 	jp	Start
 
@@ -119,54 +93,49 @@ JOYPAD_VECT:
 		;0123456789A
 
 	; $013F-$0142 (Product code - 4 ASCII characters, assigned by Nintendo, just leave blank)
-	DB	"    "
+	db	"    "
 		;0123
 
 	; $0143 (Color GameBoy compatibility code)
-	DB	$00	; $00 - DMG
+	db	$00	; $00 - DMG
 			; $80 - DMG/GBC
 			; $C0 - GBC Only cartridge
 
 	; $0144 (High-nibble of license code - normally $00 if $014B != $33)
-	DB	$00
+	db	$00
 
 	; $0145 (Low-nibble of license code - normally $00 if $014B != $33)
-	DB	$00
+	db	$00
 
 	; $0146 (GameBoy/Super GameBoy indicator)
-	DB	$00	; $00 - GameBoy
+	db	$00	; $00 - GameBoy
 
 	; $0147 (Cartridge type - all Color GameBoy cartridges are at least $19)
-	DB	$1B ; $1B - ROM + MBC5 + RAM + BATT
+	db	$1B ; $1B - ROM + MBC5 + RAM + BATT
 
 	; $0148 (ROM size)
-	DB	$01	; $01 - 512Kbit = 64Kbyte = 4 banks
+	db	$01	; $01 - 512Kbit = 64Kbyte = 4 banks
 
 	; $0149 (RAM size)
-	DB	$04 ;4 -   1MBit =128kB =16 banks
+	db	$04 ;4 -   1MBit =128kB =16 banks
 
 	; $014A (Destination code)
-	DB	$01	; $01 - All others
+	db	$01	; $01 - All others
 			; $00 - Japan
 
 	; $014B (Licensee code - this _must_ be $33)
-	DB	$33	; $33 - Check $0144/$0145 for Licensee code.
+	db	$33	; $33 - Check $0144/$0145 for Licensee code.
 
 	; $014C (Mask ROM version - handled by RGBFIX)
-	DB	$00
+	db	$00
 
 	; $014D (Complement check - handled by RGBFIX)
-	DB	$00
+	db	$00
 
 	; $014E-$014F (Cartridge checksum - handled by RGBFIX)
-	DW	$00
+	dw	$00
 
-
-;****************************************************************************************************************************************************
-;*	Program Start
-;****************************************************************************************************************************************************
-
-SECTION "Program Start",HOME[$0150]
+SECTION "Program Start", HOME[$0150]
 
 Farcall::
 ; Call a:hl.
